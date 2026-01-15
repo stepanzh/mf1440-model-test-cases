50875a02191638cbb0addcb18c5bde381684f651
stepanzh <stepanzh@gmail.com>
2026-01-15 11:50:17 +0300

    refactor(py/example): main function, quotes

--- py/example.py
+++ py/example.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python3
 
-from constellation import *
+from constellation import Constellation
+import numpy as np
 from random import randint
+import sys
 
-# создание объекта типа Constellation, инициализация параметрами группировки Stalink из конфига
-constellation = Constellation('Starlink')
 
-# вычисление элементов орбиты для всех КА в начальный момент
-constellation.getInitialState()
+def main(args):
+    constellation = Constellation('Starlink')
 
-# определение точек на оси времени, в которые будут проихзводиться расчёты
-epochs = list(range(1002))
+    # вычисление элементов орбиты для всех КА в начальный момент
+    constellation.getInitialState()
 
-# расчёт положений всех КА в заданные моменты времени
-constellation.propagateJ2(epochs)
+    # определение точек на оси времени, в которые будут проихзводиться расчёты
+    epochs = list(range(1002))
 
-# Координаты случайного КА (в инерциальных осях) после этого можно прочитать из constellation.stateEci
-satIdx = randint(0, constellation.totalSatCount - 1)
-epochIdx = randint(0, len(epochs) - 1)
-print("Положение КА-" + str(satIdx) + " на эпоху " + str(epochs[epochIdx]) + ":")
-print(constellation.stateEci[satIdx, :, epochIdx])
+    # расчёт положений всех КА в заданные моменты времени
+    constellation.propagateJ2(epochs)
+
+    # Координаты случайного КА (в инерциальных осях) после этого можно прочитать из constellation.stateEci
+    satIdx = randint(0, constellation.totalSatCount - 1)
+    epochIdx = randint(0, len(epochs) - 1)
+
+    print('Положение КА-' + str(satIdx) + ' на эпоху ' + str(epochs[epochIdx]) + ':')
+    print(constellation.stateEci[satIdx, :, epochIdx])
+
+
+if __name__ == '__main__':
+    main(sys.argv)
